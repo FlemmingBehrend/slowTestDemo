7b61b2ba0f1219385aa752a8bd46da5cee69c1c4
FlemmingBehrend <feb@topdanmark.dk>
2016-12-15 23:05:35 +0100

    modified tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,18 +1,19 @@
 /* tslint:disable:no-unused-variable */
-import {TestBed, async} from '@angular/core/testing';
-import {AppComponent} from './app.component';
-import {MaterialModule} from '@angular/material';
-import {ToolbarComponent} from './toolbar/toolbar.component';
-import {ConsequencesComponent} from './consequences/consequences.component';
-import {StakeholderComponent} from './stakeholder/stakeholder.component';
-import {AffectedInsuranceComponent} from './affected-insurance/affected-insurance.component';
-import {HeaderInfoComponent} from './header-info/header-info.component';
-import {PriceInfoComponent} from './price-info/price-info.component';
-import {ActionListComponent} from './action-list/action-list.component';
-import {ErrorComponent} from './error/error.component';
-import {ConsequenceService} from './consequence.service';
-import {ApiConfigService} from './api-config.service';
-import {WindowRefService} from './window-ref.service';
+import {TestBed, async} from "@angular/core/testing";
+import {AppComponent} from "./app.component";
+import {ToolbarComponent} from "./toolbar/toolbar.component";
+import {ConsequencesComponent} from "./consequences/consequences.component";
+import {StakeholderComponent} from "./stakeholder/stakeholder.component";
+import {AffectedInsuranceComponent} from "./affected-insurance/affected-insurance.component";
+import {HeaderInfoComponent} from "./header-info/header-info.component";
+import {PriceInfoComponent} from "./price-info/price-info.component";
+import {ActionListComponent} from "./action-list/action-list.component";
+import {ErrorComponent} from "./error/error.component";
+import {ConsequenceService} from "./consequence.service";
+import {ApiConfigService} from "./api-config.service";
+import {WindowRefService} from "./window-ref.service";
+import {NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA} from "@angular/core";
+import {MaterialModule} from "@angular/material";
 
 describe('AppComponent', () => {
 
@@ -29,13 +30,16 @@ describe('AppComponent', () => {
                 ActionListComponent,
                 ErrorComponent
             ],
-            imports: [
-                MaterialModule.forRoot()
-            ],
             providers: [
                 ConsequenceService,
                 ApiConfigService,
                 WindowRefService
+            ],
+            imports: [
+                MaterialModule.forRoot()
+            ],
+            schemas: [
+              NO_ERRORS_SCHEMA
             ]
         });
     });
--- src/app/toolbar/toolbar.component.spec.ts
+++ src/app/toolbar/toolbar.component.spec.ts
@@ -1,36 +1,33 @@
 ///<reference path="../../../node_modules/@angular/core/src/debug/debug_node.d.ts"/>
 /* tslint:disable:no-unused-variable */
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-import {MaterialModule} from '@angular/material';
-import {ToolbarComponent} from './toolbar.component';
-import {By} from '@angular/platform-browser';
+import {ComponentFixture, TestBed} from "@angular/core/testing";
+import {ToolbarComponent} from "./toolbar.component";
+import {By} from "@angular/platform-browser";
+import {NO_ERRORS_SCHEMA} from "@angular/core";
 
 describe('ToolbarComponent', () => {
 
-    let component: ToolbarComponent;
-    let fixture: ComponentFixture<ToolbarComponent>;
+  let component: ToolbarComponent;
+  let fixture: ComponentFixture<ToolbarComponent>;
 
-    beforeEach(async(() => {
-        TestBed.configureTestingModule({
-            declarations: [
-                ToolbarComponent
-            ],
-            imports: [
-                MaterialModule.forRoot()
-            ]
-        }).compileComponents();
-    }));
-
-    beforeEach(() => {
-        fixture = TestBed.createComponent(ToolbarComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        ToolbarComponent
+      ],
+      schemas: [
+        NO_ERRORS_SCHEMA
+      ]
     });
+    fixture = TestBed.createComponent(ToolbarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-    it('should create a toolbar with a header', () => {
-        fixture = TestBed.createComponent(ToolbarComponent);
-        let toolbar = fixture.debugElement.query(By.css('md-toolbar'));
-        expect(toolbar).toBeDefined();
-    });
+  it('should create a toolbar with a header', () => {
+    fixture = TestBed.createComponent(ToolbarComponent);
+    let toolbar = fixture.debugElement.query(By.css('p-toolbar'));
+    expect(toolbar.nativeElement.innerHTML).toBe('Konsekvens');
+  });
 
 });
